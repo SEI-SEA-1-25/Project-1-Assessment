2f192f6ea7e047ac168b508857d04f32b79adce5
gabrielbustos550 <gabrielbustos550@gmail.com>
2021-02-12 15:31:39 -0700

    week 3 assesment

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let add = document.querySelector('#add');
+let subtract = document.querySelector('#subtract');
+let input = document.querySelector('#input');
+let result = document.querySelector('#result')
+
+
+
+
+
+
+add.addEventListener('click', addToInput)
+
+
+function addToInput(){
+    result.innerText = input.value + result.value
+
+
+
+}
+
+
