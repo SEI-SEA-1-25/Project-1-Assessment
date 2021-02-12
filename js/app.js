2642eaa862d70a953696fa65d5f55f16426fc357
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-02-12 14:07:34 -0800

    still cant access input value

--- js/app.js
+++ js/app.js
@@ -1,19 +1,17 @@
 console.log('linked')
 /* Constants */
 const num = document.querySelector("#input").value;
+const countDisplay = document.querySelector('#count-display').value;
 //setting input value to 1 to begin
 document.querySelector("#input").value = 1;
-/* Game Logic Variables and State */
-
-/* DOM References */
-
-/* Functions and Game Logic */
-
+console.log(document.querySelector('h1').value)
 /* Event Listeners */
-document.getElementById('plus').addEventListener('click', function(){
-    document.querySelector('#count-display').value = num + document.querySelector('#input').value
+document.querySelector('#plus').addEventListener('click', function(){
+    let newNum = countDisplay + num;
+    document.querySelector('#count-display').value = newNum;
 })
 
-// document.getElementById('minus').addEventListener('click', function(){
-//     value = value - num 
-// })
+document.getElementById('minus').addEventListener('click', function(){
+    let newNum = countDisplay - num;
+    document.querySelector('#count-display').value = newNum;
+})
