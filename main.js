88a2dff7dbd76b3aa5dceea301cd22e62f7c1cc8
semuel1 <semuelst1@gmail.com>
2021-02-12 15:13:18 -0800

    p1

---
+++ main.js
@@ -0,0 +1,14 @@
+let plusButton = document.querySelector('#plus')
+let minusButton = document.querySelector('#minus')
+let idCounter = document.querySelector('#id-counter')
+
+
+//
+let count = 0
+
+//
+
+plusButton.addEventListener('click',function() {
+    counter++;
+    plusButton
+});
